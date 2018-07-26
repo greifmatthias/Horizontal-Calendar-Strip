c205985082665a4722b962b43ed563e8a5e21d8f
greif.matthias <greif.matthias@outlook.com>
2018-07-26 23:13:31 +0200

    Github button added

--- HorizontalCalendar/app/src/main/java/be/greifmatthias/horizontalcalendar/MainActivity.java
+++ HorizontalCalendar/app/src/main/java/be/greifmatthias/horizontalcalendar/MainActivity.java
@@ -1,8 +1,11 @@
 package be.greifmatthias.horizontalcalendar;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import be.greifmatthias.horizontalcalendarstrip.HorizontalCalendar;
@@ -15,18 +18,33 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        Calendar
         final HorizontalCalendar calendar = (HorizontalCalendar)findViewById(R.id.hcCalendar);
 
         calendar.setTouchHandler(new RecyclerViewTouchHandler.ClickListener() {
             @Override
             public void onClick(View view, int position) {
                 TextView t = (TextView)findViewById(R.id.tvTest);
-                t.setText(calendar.getSelected().toLocaleString() + " clicked.");
+                if(calendar.getSelected() != null) {
+                    t.setText(calendar.getSelected().toLocaleString());
+                }else{
+                    t.setText("Rip!!");
+                }
             }
 
             @Override
             public void onLongClick(View view, int position) {
 
+            }
+        });
+
+//        Github button
+        Button btnGit = findViewById(R.id.btnGit);
+        btnGit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/greifmatthias/Horizontal-Calendar-Strip"));
+                startActivity(browserIntent);
             }
         });
     }
