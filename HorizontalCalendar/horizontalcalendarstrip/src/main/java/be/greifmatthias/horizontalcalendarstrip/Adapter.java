5e617f7e5358dff6e7bc49776acd3e1bd134da37
greif.matthias <greif.matthias@outlook.com>
2018-07-28 14:49:56 +0200

    Introducing TileLayout

--- HorizontalCalendar/horizontalcalendarstrip/src/main/java/be/greifmatthias/horizontalcalendarstrip/Adapter.java
+++ HorizontalCalendar/horizontalcalendarstrip/src/main/java/be/greifmatthias/horizontalcalendarstrip/Adapter.java
@@ -2,26 +2,33 @@ package be.greifmatthias.horizontalcalendarstrip;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
-import java.text.DateFormatSymbols;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
-import java.util.Locale;
+
+import be.greifmatthias.horizontalcalendarstrip.View.TileLayout;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.DateViewHolder> {
     private List<DateItem> _dates = new ArrayList<>();
     private Context _context;
 
-    public Adapter(List<DateItem> items, Context context) {
+    private TileLayout _tileLayout;
+
+    public Adapter(List<DateItem> items, Context context, @Nullable TileLayout tileLayout) {
         this._dates = items;
         this._context = context;
+
+        if(tileLayout != null){
+            this._tileLayout = tileLayout;
+        }else{
+            this._tileLayout = new TileLayout();
+        }
     }
 
     @NonNull
@@ -34,28 +41,16 @@ public class Adapter extends RecyclerView.Adapter<Adapter.DateViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull DateViewHolder holder, final int position) {
-        DateItem item = getItem(position);
-
-//            Get date
-        Calendar c = item.getDate();
-
-//            Set Text
-        holder._tvDate.setText(String.valueOf(c.get(Calendar.DAY_OF_MONTH)));
-
-        DateFormatSymbols dfs = new DateFormatSymbols(Locale.getDefault());
-        holder._tvDay.setText(dfs.getShortWeekdays()[c.get(Calendar.DAY_OF_WEEK)]);
-
-//            Set backgroundcolor if selected
-        if(item.isSelected()){
-            holder._llRoot.setBackgroundColor(_context.getResources().getColor(R.color.Dark));
-            holder._tvDate.setTextColor(_context.getResources().getColor(R.color.White));
-            holder._tvDay.setTextColor(_context.getResources().getColor(R.color.White_semitrans));
+//        Load tilelayout
+        holder._flContent.removeAllViewsInLayout();
+        if(getItem(position).isSelected()){
+            holder._flContent.addView(this._tileLayout.getSelectedView(this._context));
         }else{
-//            Revert possible stylings
-            holder._llRoot.setBackgroundColor(_context.getResources().getColor(R.color.Dark_lighttrans));
-            holder._tvDate.setTextColor(_context.getResources().getColor(R.color.Dark));
-            holder._tvDay.setTextColor(_context.getResources().getColor(R.color.Dark_semitrans));
+            holder._flContent.addView(this._tileLayout.getDefaultView(this._context));
         }
+
+//        Bind information to view
+        this._tileLayout.bind(holder._flContent, getItem(position), getItem(position).isSelected());
     }
 
     @Override
@@ -108,17 +103,12 @@ public class Adapter extends RecyclerView.Adapter<Adapter.DateViewHolder> {
     }
 
     public class DateViewHolder extends RecyclerView.ViewHolder {
-        private TextView _tvDate;
-        private TextView _tvDay;
-        private LinearLayout _llRoot;
+        private FrameLayout _flContent;
 
         public DateViewHolder(View itemView) {
             super(itemView);
 
-//            Get controls
-            this._tvDate = (TextView)itemView.findViewById(R.id.tvDate);
-            this._tvDay = (TextView)itemView.findViewById(R.id.tvDay);
-            this._llRoot = (LinearLayout) itemView.findViewById(R.id.llRoot);
+            this._flContent = (FrameLayout)itemView.findViewById(R.id.flContent);
         }
     }
 }
