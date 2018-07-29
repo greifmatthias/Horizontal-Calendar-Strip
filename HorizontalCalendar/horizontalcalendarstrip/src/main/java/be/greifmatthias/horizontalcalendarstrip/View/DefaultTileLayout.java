093478a03e2d2f1225b4aec300e6e25ace35d581
greif.matthias <greif.matthias@outlook.com>
2018-07-29 10:55:14 +0200

    TileLayout revision

--- HorizontalCalendar/horizontalcalendarstrip/src/main/java/be/greifmatthias/horizontalcalendarstrip/View/DefaultTileLayout.java
+++ HorizontalCalendar/horizontalcalendarstrip/src/main/java/be/greifmatthias/horizontalcalendarstrip/View/DefaultTileLayout.java
@@ -1,6 +1,5 @@
 package be.greifmatthias.horizontalcalendarstrip.View;
 
-import android.content.Context;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.TextView;
@@ -13,41 +12,13 @@ import be.greifmatthias.horizontalcalendarstrip.DateItem;
 import be.greifmatthias.horizontalcalendarstrip.R;
 
 public class DefaultTileLayout extends TileLayout {
-    private int _selected;
-    private int _default;
 
     public DefaultTileLayout(){
-        this._selected = 0;
-        this._default = 0;
+        super();
     }
 
     public DefaultTileLayout(@Nullable int selectedLayout, @Nullable int defaultLayout){
-        this._selected = selectedLayout;
-        this._default = defaultLayout;
-    }
-
-    public int getSelectedLayout(){
-        if(this._selected != 0) {
-            return this._selected;
-        }
-
-        return R.layout.tile_day_selected;
-    }
-
-    public int getDefaultLayout(){
-        if(this._default != 0) {
-            return this._default;
-        }
-
-        return R.layout.tile_day_default;
-    }
-
-    public View getSelectedView(Context context){
-        return View.inflate(context, getSelectedLayout(), null);
-    }
-
-    public View getDefaultView(Context context){
-        return View.inflate(context, getDefaultLayout(), null);
+        super(selectedLayout, defaultLayout);
     }
 
     public void bind(View tile, DateItem item, boolean selected){
--- HorizontalCalendar/horizontalcalendarstrip/src/main/java/be/greifmatthias/horizontalcalendarstrip/View/TileLayout.java
+++ HorizontalCalendar/horizontalcalendarstrip/src/main/java/be/greifmatthias/horizontalcalendarstrip/View/TileLayout.java
@@ -4,11 +4,36 @@ import android.content.Context;
 import android.view.View;
 
 import be.greifmatthias.horizontalcalendarstrip.DateItem;
+import be.greifmatthias.horizontalcalendarstrip.R;
 
 public abstract class TileLayout {
-    public abstract int getSelectedLayout();
-    public abstract int getDefaultLayout();
-    public abstract View getSelectedView(Context context);
-    public abstract View getDefaultView(Context context);
+    private int _defaultLayout;
+    private int _selectedLayout;
+
+//    Default constructor
+    public TileLayout(){ }
+
+//    Constructor for custom tile layout
+    public TileLayout(int selectedLayout, int defaultLayout){
+        this._defaultLayout = defaultLayout;
+        this._selectedLayout = selectedLayout;
+    }
+
+    public View getSelectedView(Context context){
+        if(this._selectedLayout != 0) {
+            return View.inflate(context, this._selectedLayout, null);
+        }
+
+        return View.inflate(context, R.layout.tile_day_selected, null);
+    }
+
+    public View getDefaultView(Context context){
+        if(this._defaultLayout != 0) {
+            return View.inflate(context, this._defaultLayout, null);
+        }
+
+        return View.inflate(context, R.layout.tile_day_default, null);
+    }
+
     public abstract void bind(View tile, DateItem item, boolean selected);
 }
